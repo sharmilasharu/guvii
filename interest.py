8177260450be095a8cd5308422645078a6b3b984
sharmila <43746533+sharmilasharu@users.noreply.github.com>
2019-05-24 09:21:16 -0700

    Create interest.py

---
+++ interest.py
@@ -0,0 +1,15 @@
+#PF-Assgn-4
+#This verification is based on string match.
+
+principal=2000
+rate_of_interest=5
+time=5
+interest=0
+
+#Start writing your code from here
+#Populate the variable: interest
+
+
+
+#Do not modify the below print statement for verification to work
+print(interest)
