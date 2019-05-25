a263084c194f2310508ebee123a446b76719afb5
sharmila <43746533+sharmilasharu@users.noreply.github.com>
2019-05-25 01:40:29 -0700

    Create oop_con.py

---
+++ oop_con.py
@@ -0,0 +1,12 @@
+class human():
+  def __init__(self,name,age,colour):
+    self.name=name
+    self.age=age
+    self.colour=colour
+var=human("hema",14,"blue")    
+print(var)
+print(var.name)
+print(var.age)
+print(var.colour)
+
+    
