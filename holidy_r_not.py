6694c04b813504b60b167535933220b7af7f58f8
sharmila <43746533+sharmilasharu@users.noreply.github.com>
2019-06-02 05:56:50 -0700

    Update holidy_r_not.py

--- holidy_r_not.py
+++ holidy_r_not.py
@@ -1,4 +1,4 @@
-input_day=input("")
+input_day=input()
 if(input_day=="saturday" or input_day=="sunday"):
   print("yes")
 elif(input_day=="monday" or input_day=="tuesday" or input_day=="wednesday" or input_day=="thursday" or input_day=="friday"):
